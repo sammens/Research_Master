7808401451f2ab63332b1276fd2097bd5c8ffba2
Samuel Ofosu Mensah <samuelmensah@aims.ac.za>
2018-01-30 10:15:22 +0200

    removed function to plot histogram

--- src/preprocessing.R
+++ src/preprocessing.R
@@ -27,19 +27,6 @@ Miller = function(x){
     return(decision)
 }
 
-"""A function to plot multiple histograms for the selected genes"""
-histplot = function(x, r, co, st){
-    #where x is the input is a matrix and is the new data after we have removed the unwanted features
-    #r is the number of graphs we want on a row
-    #co is the number of graphs we want on a column
-    #st is which index to start
-    #sp is which index to stop
-    par(mfrow = c(r,co))
-    for(i in st:(st + 19)){
-        graphs = hist(x[,i])
-    }
-    graphs
-}
 
 """A function to return all written functions"""
 all.func = function(x){
