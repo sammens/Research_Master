142e935cacb6377bd65df3d5777e1337ff8958a6
Samuel Ofosu Mensah <samuelmensah@aims.ac.za>
2018-01-30 10:13:54 +0200

    provided an util to plot histograms

---
+++ src/utils.R
@@ -0,0 +1,17 @@
+""""Contributor: Samuel Ofosu Mensah"""
+
+"""A function to plot multiple histograms"""
+histplot = function(x, r, co, st){
++     #where x is the input is a matrix and is the new data after we have removed the unwanted features
++     #r is the number of graphs we want on a row
++     #co is the number of graphs we want on a column
++     #st is which index to start
++     #sp is which index to stop
++     par(mfrow = c(r,co))
++     for(i in st:(st + 19)){
++         graphs = hist(x[,i])
++     }
++     graphs
++ 
++ }
+
