d2ee75d91576ec3fa767d1782a792b2d8fdeda30
Samuel Ofosu Mensah <samuelmensah@aims.ac.za>
2018-02-01 08:29:51 +0200

    made changes to the indentations

--- src/preprocessing.R
+++ src/preprocessing.R
@@ -10,22 +10,23 @@ CV = function(x){
 
 """A function to remove all columns with mean == 0"""
 remove_NaN = function(x){
-    #where x is the input is a matrix
-    cv = apply(x,2,CV)	#applying the CV(x) function
-    x = x[,-which(cv == 'NaN')] 	#remove all features with mean == 0
-    return(x)
+  #where x is the input is a matrix
+  cv = apply(x,2,CV)	#applying the CV(x) function
+  x = x[,-which(cv == 'NaN')] 	#remove all features with mean == 0
+  return(x)
 }
 
 """A function for Miller's test statistic"""
 Miller = function(x){
-    #where x is the input is a matrix
-    c_0 = 1/3	 	#c_0 to represent population Coefficient of Variation
-    m = length(x) - 1	#length of sample
-    cv = CV(x) 		#using the Coefficient of Variation function
-    z = ((sqrt(m)) * (cv - c_0))/(sqrt(c_0 * (0.5 + c_0**2))) #Miller's statistic
-    decision = (1 - pnorm(z)) > 0.05	#deciding null hypothesis 
-    return(decision)
+  #where x is the input is a matrix
+  c_0 = 1/3	 	#c_0 to represent population Coefficient of Variation
+  m = length(x) - 1	#length of sample
+  cv = CV(x) 		#using the Coefficient of Variation function
+  z = ((sqrt(m)) * (cv - c_0))/(sqrt(c_0 * (0.5 + c_0**2))) #Miller's statistic
+  decision = (1 - pnorm(z)) > 0.05	#deciding null hypothesis 
+  return(decision)
 }
+
 
 """A function to round-up real numbers to integers"""
 rnd = function(x){
@@ -53,9 +54,9 @@ gen_ydata = function(x){
 
 """A function to return all written functions"""
 all.func = function(x){
-    #using remove_NaN to remove all columns with mean == 0
-    x = remove_NaN(x)
-    #applying Miller's test after removing coloumns with mean == 0
-    mil = apply(x,2,Miller)
-    return(list('data' = x, 'Miller' = mil))
+  #using remove_NaN to remove all columns with mean == 0
+  x = remove_NaN(x)
+  #applying Miller's test after removing coloumns with mean == 0
+  mil = apply(x,2,Miller)
+  return(list('data' = x, 'Miller' = mil))
 }
