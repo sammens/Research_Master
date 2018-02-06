737a6e27ce196b62b24cea33dd301096350a3788
Samuel Ofosu Mensah <samuelmensah@aims.ac.za>
2018-02-06 15:58:29 +0200

    add function to clean data

--- src/preprocessing.R
+++ src/preprocessing.R
@@ -2,19 +2,33 @@
 #  Contributor: Samuel Ofosu Mensah
 ##############################################################
 
+### A function to clean TCGA dataset
+clean_data = function(x){
+  # input is unprocessed data from TCGA
+  BrcaHeader = names(x)		#names of data
+  Patient_Id = BrcaHeader[1]	#Patient ID
+  BrcaHeader = BrcaHeader[-1]	#gene names
+  BrcaHeader = unlist(strsplit(BrcaHeader, '|', fixed=T))  #splitting characters on either sides of '|' in the header
+  BrcaHeader = BrcaHeader[seq(1,length(BrcaHeader),2)]	#removing numbers
+  names(x) = c(Patient_Id, BrcaHeader)	#changing the header names of the data
+  x = x[, !grepl("\\?", names(x))]	#removing all genes starting with "?"
+  return(x)
+}
+
+
 ### A function for Coefficient of Variation
 CV = function(x){
-  #where x is the input is a matrix
-  #sd for standard deviation
+  # where x is the input is a matrix
+  # CV stands for Coefficient of Variation
   cv = (sd(x)/mean(x))
   return(cv)
 }
 
 ### A function to remove all columns with mean == 0
 remove_NaN = function(x){
-  x = data.frame(x)
   #where x is the input is a matrix
-  cv = apply(x,2,CV)	#applying the CV(x) function
+  x = data.frame(x)
+  cv = apply(x,2,CV)	   	#applying the CV(x) function
   x = x[,-which(cv == 'NaN')] 	#remove all features with mean == 0
   return(x)
 }
@@ -22,9 +36,9 @@ remove_NaN = function(x){
 ### A function for Miller's test statistic
 Miller = function(x){
   #where x is the input is a matrix
-  c_0 = 1/3	 	#c_0 to represent population Coefficient of Variation
-  m = length(x) - 1	#length of sample
-  cv = CV(x) 		#using the Coefficient of Variation function
+  c_0 = 1/3	 		#c_0 to represent population Coefficient of Variation
+  m = length(x) - 1		#length of sample
+  cv = CV(x) 			#using the Coefficient of Variation function
   z = ((sqrt(m)) * (cv - c_0))/(sqrt(c_0 * (0.5 + c_0**2))) #Miller's statistic
   decision = (1 - pnorm(z)) > 0.05	#deciding null hypothesis 
   return(decision)
