e7b144fc5e9a3085b18e37cb9af8adbebdc8f321
Samuel Ofosu Mensah <samuelmensah@aims.ac.za>
2018-02-07 09:49:50 +0200

    corrected util function

--- src/utils.R
+++ src/utils.R
@@ -9,9 +9,10 @@ histplot = function(x, r, co, st){
   # co is the number of graphs we want on a column
   # st is which index to start
   # sp is which index to stop
+  x = t(x) 	#transpose data
   par(mfrow = c(r,co))
   for(i in st:(st + ((r*co)-1))){
-    graphs = hist(x[,i], xlab = paste(rownames(x)[i]), main = paste('Histogram of ', rownames(x)[i]))
+    graphs = hist(x[,i], xlab = paste(colnames(x)[i]), main = paste('Histogram of ', colnames(x)[i]))
   }
   graphs
 }
