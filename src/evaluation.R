7a9432f6fe62e8283c1609e90c22c88a078f5269
Samuel Ofosu Mensah <samuelmensah@aims.ac.za>
2018-02-10 09:59:05 +0200

    removed all the research codes from github

--- src/evaluation.R
+++ src/evaluation.R
@@ -2,8 +2,4 @@
 #  Contributor: Samuel Ofosu Mensah
 ###############################################################
 
-### Applying the SAMseq function
 
-library(samr)
-trace('samr.estimate.depth', edit = TRUE)
-samfit = SAMseq(x = mydata$x, y = mydata$y, resp.type = "Two class unpaired", geneid = mydata$geneid, genenames = mydata$genenames)
--- src/utils.R
+++ src/utils.R
@@ -2,18 +2,4 @@
 #  Contributor: Samuel Ofosu Mensah
 #################################################################
 
-### A function to plot multiple histograms
-histplot = function(x, r, co, st){
-  # where x is the input is a matrix and is the new data after we have removed the unwanted features
-  # r is the number of graphs we want on a row
-  # co is the number of graphs we want on a column
-  # st is which index to start
-  # sp is which index to stop
-  x = t(x) 	#transpose data
-  par(mfrow = c(r,co))
-  for(i in st:(st + ((r*co)-1))){
-    graphs = hist(x[,i], xlab = paste(colnames(x)[i]), main = paste('Histogram of ', colnames(x)[i]))
-  }
-  graphs
-}
 
